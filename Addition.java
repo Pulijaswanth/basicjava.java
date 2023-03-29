968ccee79f4ace1ffe04454ea356a1f141c7da0a
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-03-29 12:06:22 +0530

    Add files via upload
    Addition of two number

---
+++ Addition.java
@@ -0,0 +1,17 @@
+//addition of numbers
+import java.util.*;
+class Addition
+{
+public  static void main(String[] args)
+{
+int no1,no2,res;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the first number");
+no1=sc.nextInt();
+System.out.println("enter the second number");
+no2=sc.nextInt();
+res=no1+no2;
+System.out.println("sum of "+no1+" and "+no2+" is" +res);
+}
+}
+
