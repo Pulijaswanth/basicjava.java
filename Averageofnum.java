546c105651fdfa4eb8321ca096b02f118e89ee9a
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-03-31 23:21:17 +0530

    Add files via upload

---
+++ Averageofnum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Averageofnum
+{
+      public static void main(String[] args)
+{
+     int a,b,c,sum,average;
+     Scanner sc=new Scanner(System.in);
+	 System.out.println("enter the first number");
+	 a=sc.nextInt();
+	 System.out.println("enter the second number");
+	 b=sc.nextInt();
+	 System.out.println("enter the three number");
+	 c=sc.nextInt();
+	 sum=a+b+c;
+	 average=sum/3;
+	 System.out.println("the sum is "+sum+ " and average is "+average+"");
+}
+}
+	 
+	 
