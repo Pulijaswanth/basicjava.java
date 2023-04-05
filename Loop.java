8a0d6963b24e8607a1c943ab81565c2b9d68a651
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-04-05 11:05:38 +0530

    Add files via upload
    To demonstrate the concept of Loop.

---
+++ Loop.java
@@ -0,0 +1,15 @@
+//demo on loop
+class Loop
+{
+       public static void main(String[] args)
+      {
+       int count =1;
+        while(count<=5)
+         {
+        System.out.println("name");
+        count++;
+         }
+   }
+}
+		
+	   
